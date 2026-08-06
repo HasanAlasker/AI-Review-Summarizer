80b26f5c62c9a5fa77fe8edd7fd45a8dae03f183
Hasan Alasker <hasanalasker2004@gmail.com>
2026-08-06 20:12:06 +0300

    gird adjutments

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: "#ffff",
+  themeColor: "#ffffff",
 };
 
 export default function RootLayout({
--- components/product/Card.tsx
+++ components/product/Card.tsx
@@ -41,7 +41,7 @@ export default function Card({
       <CardFooter>
         <div className="flex w-full items-center justify-between">
           <p>{"$" + price.toString()}</p>
-          <Button>
+          <Button >
             <Link href={`/products/${id}`}>Learn More</Link>
           </Button>
         </div>
