aa21416f1dd196626beaeeab608ffe8066bab2d7
Hasan Alasker <hasanalasker2004@gmail.com>
2026-08-09 13:45:59 +0300

    role added to nextauth types

--- components/general/NavBar.tsx
+++ components/general/NavBar.tsx
@@ -2,11 +2,10 @@
 import { useTheme } from "@/app/store/useTheme";
 import { ButtonGroup } from "@/components/ui/button-group";
 import {
-  CircleUser,
-  Home,
   LogOut,
   Moon,
   ShoppingCart,
+  Store,
   Sun,
   Undo2,
   WalletCards,
@@ -16,32 +15,34 @@ import { usePathname } from "next/navigation";
 import NavBtn from "./NavBtn";
 
 const HIDE_BACK_ON = ["/products", "/"];
-const HIDE_HOME_ON = ["/"];
 
 export default function NavBar() {
   const isDark = useTheme((state) => state.isDark);
   const pathname = usePathname();
 
-  const { data: user, status } = useSession();
+  const { data: session, status } = useSession();
+  console.log(session?.user!.role);
 
   return (
     <nav className="z-50 flex w-full mx-auto justify-between py-4 lg:py-8 top-0 right-0 left-0 sticky bg-background/50 backdrop-blur-md self-start h-fit">
       <ButtonGroup>
-        {!HIDE_HOME_ON.includes(pathname) && (
-          <NavBtn path="/" children={<Home />} />
-        )}
+        <NavBtn path="/products" children={<Store />} />
+
         {!HIDE_BACK_ON.includes(pathname) && (
           <NavBtn back children={<Undo2 />} />
         )}
-        {pathname === "/" && <NavBtn path="/products" children={"Shop"} />}
       </ButtonGroup>
 
       <ButtonGroup>
         {status === "unauthenticated" && (
           <NavBtn path="/api/auth/signin" children={"Sign in"} />
         )}
-        <NavBtn path="/cart" children={<ShoppingCart />} />
-        <NavBtn path="/orders" children={<WalletCards />} />
+        {session?.user.role === "user" && (
+          <NavBtn path="/cart" children={<ShoppingCart />} />
+        )}
+        {session?.user.role === "admin" && (
+          <NavBtn path="/orders" children={<WalletCards />} />
+        )}
 
         <NavBtn toggleTheme children={!isDark ? <Moon /> : <Sun />} />
         {status === "authenticated" && (
