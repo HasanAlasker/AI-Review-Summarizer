d24a6e637eafc3afa43000c2e67b8e17d86e5677
Hasan Alasker <hasanalasker2004@gmail.com>
2026-08-10 00:39:58 +0300

    trying to make build work

--- app/api/admin/product/[id]/route.ts
+++ app/api/admin/product/[id]/route.ts
@@ -1,2 +1,8 @@
 // edit product
+
+import { NextRequest, NextResponse } from "next/server";
+
 // delete product
+export async function PUT(rea: NextRequest) {
+  return NextResponse.json({ ok: true });
+}
